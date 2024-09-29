b8f46611f66813b96f6b37c3a0ef538e2583a0b5
jonmc89 <126274870+jonmc89@users.noreply.github.com>
2024-09-29 14:30:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-user_prompt = 'Enter a todo:'
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-todos = [todo1, todo2, todo3]
-print(todos)
+user_prompt = "Enter a todo:"
+
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
+    print(todos)
+    
+
